2910ea7735da9abae78216cf277b58d281e7ca9c
Shivam Raj Singh <48837530+shivam3009@users.noreply.github.com>
2021-08-15 21:45:06 +0530

    15-8-21

---
+++ bubble_sort.cpp
@@ -0,0 +1,20 @@
+#include "codeblock.h"
+
+void bubbleSort (vector <int> &arr){
+    int n = arr.size();
+    for(int i =0; i < n-1; i++){
+        for(int j = 0; j < n-1-i; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+
+
+int main(){
+    vector <int> arr = input();
+    printBeforeSorting(arr);
+    bubbleSort(arr);
+    printAfterSorting(arr);
+}
---
+++ codeblock.h
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> input (){
+    int n;
+    cout << "Enter size of an array : ";
+    cin >> n;
+    vector <int> arr(n);
+    cout << "Enter Elements : ";
+    for(int i =0; i < n; i++){
+        cin >> arr[i];
+    }
+    return arr;
+}
+
+void printBeforeSorting (vector<int> arr){
+    int n = arr.size();
+    cout << "Before Sorting : ";
+    for(int i =0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+void printAfterSorting (vector<int> arr){
+    int n = arr.size();
+    cout << "After Sorting : ";
+    for(int i =0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
