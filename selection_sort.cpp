828f5c02767006c7462e320ff68734b807d3d2e4
Shivam Raj Singh <48837530+shivam3009@users.noreply.github.com>
2021-08-15 22:00:03 +0530

    15-8-21

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,6 +1,20 @@
 #include "codeblock.h"
 void selectionSort (vector <int> &arr){
-    int n = 
+    int n = arr.size();
+    int min = 0;
+    for(int i = 0; i < n-1; i++){
+        min = i;
+        for(int j = i+1; j < n; j++){
+            if(arr[j] < arr[min]){
+                min = j;
+            }
+        }
+        if(min != i){
+            swap(arr[i],arr[min]);
+        }
+    }
+    // Time Complexity : O(n^2)
+    // Space Complexity : O(1)
 }
 int main()
 {
